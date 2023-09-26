b400f466d37f036ea7139424061c6acd9772fd36
gabezy <gabrielmoreira2705@gmail.com>
2023-09-26 00:05:49 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-purple-200": "#9f23fe",
+        "custom-purple-500": "#9c76f5",
+        "custom-purple-700": "#7758bf",
+      }
+    },
+  },
+  plugins: [],
+}
