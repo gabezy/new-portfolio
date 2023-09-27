cad7d98865c73e9530cbd6e407c56aaf363ba579
gabezy <gabrielmoreira2705@gmail.com>
2023-09-26 21:13:49 -0300

    feature: custom setting tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
         "custom-purple-200": "#9f23fe",
         "custom-purple-500": "#9c76f5",
         "custom-purple-700": "#7758bf",
+      },
+      spacing: {
+        "column": "500px",
       }
     },
   },
